092aa9b72a7f1438589102a6a5dd78ea5b022882
watson <chikuwa.wachi@gmail.com>
2020-10-19 07:58:26 +0900

    [change,add]jQueryを導入し、レスポンシブハンバーガーメニューを追加(テスト用にメニューは着色)。

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const {
+  environment
+} = require('@rails/webpacker')
+
+// jQueryを利用するための設定
+const webpack = require("webpack")
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery"
+  })
+)
 
 module.exports = environment
