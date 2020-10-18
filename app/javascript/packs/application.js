e5e04d2efcee33ce9587ff44d7e001faf9c9396b
watson <chikuwa.wachi@gmail.com>
2020-10-19 07:58:26 +0900

    [change,add]jQueryを導入し、レスポンシブハンバーガーメニューを追加(テスト用にメニューは着色)。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// ========== Railsデフォルト
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// ========== Railsデフォルト以外で、汎用的に使用するライブラリなどの設定
+// jQueryを使用するための設定
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
