63db24feeb25dfb54e5b5fba2bcc6b66a1127e51
watson <chikuwa.wachi@gmail.com>
2020-10-20 08:51:07 +0900

    Revert "[change,add]jQueryを導入し、レスポンシブハンバーガーメニューを追加(テスト用にメニューは着色)。"
    This reverts commit e5e04d2efcee33ce9587ff44d7e001faf9c9396b.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,3 @@
-const {
-  environment
-} = require('@rails/webpacker')
-
-// jQueryを利用するための設定
-const webpack = require("webpack")
-environment.plugins.prepend(
-  "Provide",
-  new webpack.ProvidePlugin({
-    $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery"
-  })
-)
+const { environment } = require('@rails/webpacker')
 
 module.exports = environment
