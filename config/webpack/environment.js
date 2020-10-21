db00887ebf5841f8a86a28dc1454ac0eff1b9569
watson <chikuwa.wachi@gmail.com>
2020-10-22 07:55:31 +0900

    [add,change,delete]独自定義のJavaScriptを「.js.erb]に修正し、必要なWebpacker設定を追加。

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const {
   environment
 } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 
 // jQueryを利用するための設定
 const webpack = require("webpack")
@@ -12,4 +13,5 @@ environment.plugins.prepend(
   })
 )
 
+environment.loaders.prepend('erb', erb)
 module.exports = environment
