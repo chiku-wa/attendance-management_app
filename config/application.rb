d5986c4dde0d278cc57240c7f1f635bad7f76ccb
watson <chikuwa.wachi@gmail.com>
2021-03-09 08:25:10 +0900

    [add]RSpecテストを自動生成する設定を追加。

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,5 +15,24 @@ module AttendanceManagementApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # 「rails generate」コマンド実行時に自動的にRSPecテストファイルが生成されるようにする
+    config.generators do |g|
+      g.test_framework(
+        :rspec,
+
+        # テストデータ登録用のフィクスチャファイルの自動生成するか否か：fixture以外の仕組みでテストデータを作成するため自動生成はしない
+        fixtures: false,
+
+        # View生成時にRSPecテストを自動生成するか否か：画面周りはfeatureテスト(Capyara等)で試験するため、自動生成はしない
+        view_specs: false,
+
+        # ヘルパーメソッド作成時にテストを自動生成するか否か：
+        helper_specs: true,
+
+        # routing.rbのテストを自動生成するか否か：spec/requests配下のテストメソッドでテストするため自動生成しない
+        routing_specs: false,
+      )
+    end
   end
 end
