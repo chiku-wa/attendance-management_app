20b45a2b4de7b0414da2c004514d6b998ce5e5fa
watson <chikuwa.wachi@gmail.com>
2021-04-12 08:39:37 +0900

    [change,add]環境ごとにseedでデータを登録できるように構成を見直し。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,6 @@
-# ===== 就業状況登録
-#
-employment_status_work = EmploymentStatus.create!(
-  status_code: "00001",
-  status_name: "在職",
-)
-employment_status_leave = EmploymentStatus.create!(
-  status_code: "00002",
-  status_name: "休職",
-)
-
-employment_status_retirement = EmploymentStatus.create!(
-  status_code: "00003",
-  status_name: "退職",
-)
-
-employment_statuses = [
-  employment_status_work,
-  employment_status_leave,
-  employment_status_retirement,
-]
-
-# ===== 社員登録
-employees = []
-
-99.times do |i|
-  # gimeiのgemを使ってランダムな名前を生成
-  gimei = Gimei.name
-
-  employees << Employee.new(
-    employee_code: "A#{format("%04d", i)}",
-    employee_name: gimei.kanji,
-    employee_name_kana: gimei.katakana,
-    age: rand(1..100),
-    employment_status: employment_statuses[
-      rand(0..(employment_statuses.size - 1))
-    ],
-  )
-end
-Employee.import!(employees)
+# db/seeds配下に存在する、環境ごとのrbを呼び出す
+# db/seeds
+# └development.rb
+# └production.rb
+# └test.rb
+load(Rails.root.join("db", "seeds", "#{Rails.env.downcase}.rb"))
---
+++ db/seeds/development.rb
@@ -0,0 +1,40 @@
+# ===== 就業状況登録
+#
+employment_status_work = EmploymentStatus.create!(
+  status_code: "00001",
+  status_name: "在職",
+)
+employment_status_leave = EmploymentStatus.create!(
+  status_code: "00002",
+  status_name: "休職",
+)
+
+employment_status_retirement = EmploymentStatus.create!(
+  status_code: "00003",
+  status_name: "退職",
+)
+
+employment_statuses = [
+  employment_status_work,
+  employment_status_leave,
+  employment_status_retirement,
+]
+
+# ===== 社員登録
+employees = []
+
+99.times do |i|
+  # gimeiのgemを使ってランダムな名前を生成
+  gimei = Gimei.name
+
+  employees << Employee.new(
+    employee_code: "A#{format("%04d", i)}",
+    employee_name: gimei.kanji,
+    employee_name_kana: gimei.katakana,
+    age: rand(1..100),
+    employment_status: employment_statuses[
+      rand(0..(employment_statuses.size - 1))
+    ],
+  )
+end
+Employee.import!(employees)
