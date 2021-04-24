69ef49e8a9c44bfaf39306a042120c207318255f
watson <chikuwa.wachi@gmail.com>
2021-04-24 11:38:28 +0900

    [fix,add,change]設計書通りのカラム構成になるように定義を修正し、各テーブル・カラムに論理名のコメントを追加。

--- db/schema.rb
+++ db/schema.rb
@@ -10,88 +10,92 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_231758) do
+ActiveRecord::Schema.define(version: 2021_04_24_013707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "affiliation_types", force: :cascade do |t|
-    t.string "affiliation_type_name", limit: 10, null: false
+  create_table "affiliation_types", comment: "所属種別", force: :cascade do |t|
+    t.string "affiliation_type_name", limit: 10, null: false, comment: "所属種別名"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["affiliation_type_name"], name: "unique_affiliation_types_on_affiliation_type_name", unique: true
   end
 
-  create_table "departments", force: :cascade do |t|
-    t.string "department_name", limit: 200, null: false
-    t.string "department_kana_name", limit: 400, null: false
-    t.datetime "establishment_date", null: false
-    t.datetime "abolished_date", null: false
+  create_table "departments", comment: "部署テーブル", force: :cascade do |t|
+    t.string "department_name", limit: 200, null: false, comment: "部署名"
+    t.string "department_kana_name", limit: 400, null: false, comment: "部署名(カナ)"
+    t.datetime "establishment_date", null: false, comment: "設立日"
+    t.datetime "abolished_date", null: false, comment: "廃止日"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "department_code", limit: 12, null: false
+    t.string "department_code", limit: 12, null: false, comment: "部署コード"
     t.index ["department_code", "establishment_date", "abolished_date"], name: "unique_departments_on_department_code_establishment_abolished", unique: true
   end
 
-  create_table "employee_departments", force: :cascade do |t|
-    t.bigint "department_id", null: false
-    t.bigint "employee_id", null: false
+  create_table "employee_departments", comment: "社員 - 部署", force: :cascade do |t|
+    t.bigint "department_id", null: false, comment: "部署ID"
+    t.bigint "employee_id", null: false, comment: "社員ID"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "affiliation_type_id"
+    t.bigint "affiliation_type_id", comment: "所属種別ID"
+    t.datetime "start_date", precision: 6, null: false, comment: "着任日"
+    t.datetime "end_date", precision: 6, null: false, comment: "離任日"
   end
 
-  create_table "employees", force: :cascade do |t|
-    t.string "employee_code", limit: 6, null: false
-    t.string "employee_name", limit: 110, null: false
-    t.string "employee_name_kana", limit: 220, null: false
-    t.integer "age", limit: 2, null: false
+  create_table "employees", comment: "社員", force: :cascade do |t|
+    t.string "employee_code", limit: 6, null: false, comment: "社員コード"
+    t.string "employee_name", limit: 110, null: false, comment: "社員名"
+    t.string "employee_name_kana", limit: 220, null: false, comment: "社員名(フリガナ)"
+    t.integer "age", limit: 2, null: false, comment: "年齢"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "employment_status_id", null: false
+    t.bigint "employment_status_id", null: false, comment: "就業状況ID"
     t.index ["employee_code"], name: "unique_employees_on_employee_code", unique: true
   end
 
-  create_table "employment_statuses", force: :cascade do |t|
-    t.string "status_name", limit: 100, null: false
-    t.string "status_code", limit: 5, null: false
+  create_table "employment_statuses", comment: "就業状況", force: :cascade do |t|
+    t.string "status_name", limit: 100, null: false, comment: "就業状況"
+    t.string "status_code", limit: 5, null: false, comment: "就業状況コード"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["status_code"], name: "unique_employment_statuses_on_status_code", unique: true
     t.index ["status_name"], name: "unique_employment_statuses_on_status_name", unique: true
   end
 
-  create_table "hierarchy_departments", force: :cascade do |t|
-    t.bigint "parent_department_id", null: false
-    t.bigint "child_department_id", null: false
+  create_table "hierarchy_departments", comment: "部署階層", force: :cascade do |t|
+    t.bigint "parent_department_id", null: false, comment: "親部署ID"
+    t.bigint "child_department_id", null: false, comment: "子親部署ID"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string "project_code", limit: 110, null: false
-    t.string "project_name", limit: 300, null: false
-    t.boolean "enabled", null: false
-    t.datetime "start_date"
-    t.datetime "end_date"
+  create_table "projects", comment: "プロジェクト", force: :cascade do |t|
+    t.string "project_code", limit: 110, null: false, comment: "プロジェクトコード"
+    t.string "project_name", limit: 300, null: false, comment: "プロジェクト名"
+    t.boolean "enabled", null: false, comment: "有効フラグ"
+    t.datetime "start_date", comment: "プロジェクト開始日"
+    t.datetime "end_date", comment: "プロジェクト終了日"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["project_code"], name: "unique_projects_on_project_code", unique: true
   end
 
-  create_table "ranks", force: :cascade do |t|
-    t.string "rank_code", limit: 2, null: false
+  create_table "ranks", comment: "ランク", force: :cascade do |t|
+    t.string "rank_code", limit: 2, null: false, comment: "ランクコード"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["rank_code"], name: "index_ranks_on_rank_code", unique: true
   end
 
-  create_table "work_tables", force: :cascade do |t|
-    t.string "employee_code", limit: 6, null: false
-    t.datetime "working_date", null: false
-    t.string "project_code", limit: 110, null: false
-    t.string "rank_code", limit: 2, null: false
+  create_table "work_tables", comment: "勤務表", force: :cascade do |t|
+    t.string "employee_code", limit: 6, null: false, comment: "就業状況コード"
+    t.datetime "working_date", null: false, comment: "勤務日"
+    t.string "project_code", limit: 110, null: false, comment: "プロジェクトコード"
+    t.string "rank_code", limit: 2, null: false, comment: "ランクコード"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "closed", null: false
+    t.boolean "closed", null: false, comment: "締めフラグ"
   end
 
   add_foreign_key "employee_departments", "affiliation_types", name: "fk_affiliation_type_id"
