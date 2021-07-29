9f33ea4902c23e45c1d97d6866cd2fd9bc9cdda6
watson <chikuwa.wachi@gmail.com>
2021-07-29 11:07:29 +0900

    [change]FactoryBotのテストデータに部署階層の登録処理を追加。

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,20 +1,25 @@
-# ===== 部署登録
+# === 部署登録
 #
-department_A = FactoryBot.build(:department_A)
-department_A_sales = FactoryBot.build(:department_A_sales)
-department_A_sales_department = FactoryBot.build(:department_A_sales_department)
-department_A_sales_department_division = FactoryBot.build(:department_A_sales_department_division)
+department_A = FactoryBot.create(:department_A)
+department_A_sales = FactoryBot.create(:department_A_sales)
+department_A_sales_department1 = FactoryBot.create(:department_A_sales_department1)
+department_A_sales_department1_division1 = FactoryBot.create(:department_A_sales_department1_division1)
+department_A_sales_department1_division2 = FactoryBot.create(:department_A_sales_department1_division2)
+department_A_sales_department2 = FactoryBot.create(:department_A_sales_department2)
+department_A_sales_department2_division1 = FactoryBot.create(:department_A_sales_department2_division1)
+department_A_sales_department2_division2 = FactoryBot.create(:department_A_sales_department2_division2)
 
-departments = [
-  department_A,
-  department_A_sales,
-  department_A_sales_department,
-  department_A_sales_department_division,
-]
+FactoryBot.create(:department_B)
 
-Department.import!(departments)
+# === 部署階層登録
+department_A.add_child(department_A_sales)
+department_A_sales.add_child(department_A_sales_department1)
+department_A_sales_department1.add_child(department_A_sales_department1_division1)
+department_A_sales_department1.add_child(department_A_sales_department1_division2)
+department_A_sales_department2.add_child(department_A_sales_department2_division1)
+department_A_sales_department2.add_child(department_A_sales_department2_division2)
 
-# ===== 就業状況登録
+# === 就業状況登録
 #
 employment_status_work = EmploymentStatus.create!(
   status_code: "00001",
@@ -36,7 +41,7 @@ employment_statuses = [
   employment_status_retirement,
 ]
 
-# ===== 社員登録
+# === 社員登録
 employees = []
 
 99.times do |i|
--- spec/factories/departments.rb
+++ spec/factories/departments.rb
@@ -11,9 +11,22 @@ FactoryBot.define do
 
   # =======================================================
   # 開発用
+  # ※「テスト用」と共用
 
   # =======================================================
   # テスト用
+  # 以下の階層構造の部署情報を想定する
+  # 以下の組織構造を持つ部署のテストデータ
+  #
+  # A事業部　A01000000000
+  # 　┗営業部　A01B01000000
+  # 　　┗第一営業部　A01B01C01000
+  # 　　　┗第一営業部　一課　　A01B01C01001
+  # 　　　┗第一営業部　二課　　A01B01C01002
+  # 　　┗第二営業部　A01B01C02000
+  # 　　　┗第ニ営業部　　一課　　A01B01C02001
+  # 　　　┗第ニ営業部　　ニ課　　A01B01C02002
+  # B事業部　B01000000000
 
   # A事業部
   factory :department_A, class: Department do
@@ -34,7 +47,7 @@ FactoryBot.define do
   end
 
   # 第一営業部
-  factory :department_A_sales_department, class: Department do
+  factory :department_A_sales_department1, class: Department do
     department_code { "A01B01C01000" }
     department_name { "第一営業部" }
     department_kana_name { "ダイイチエイギョウブ" }
@@ -42,11 +55,56 @@ FactoryBot.define do
     abolished_date { abolished_date }
   end
 
-  # 営業一課
-  factory :department_A_sales_department_division, class: Department do
+  # 第一営業部　一課
+  factory :department_A_sales_department1_division1, class: Department do
     department_code { "A01B01C01001" }
-    department_name { "営業一課" }
-    department_kana_name { "エイギョウイッカ" }
+    department_name { "第一営業部　一課" }
+    department_kana_name { "ダイイチエイギョウブ　イッカ" }
+    establishment_date { establishment_date }
+    abolished_date { abolished_date }
+  end
+
+  # 第一営業部　ニ課
+  factory :department_A_sales_department1_division2, class: Department do
+    department_code { "A01B01C01002" }
+    department_name { "第一営業部　ニ課" }
+    department_kana_name { "ダイイチエイギョウブ　ニカ" }
+    establishment_date { establishment_date }
+    abolished_date { abolished_date }
+  end
+
+  # 第二営業部
+  factory :department_A_sales_department2, class: Department do
+    department_code { "A01B01C02000" }
+    department_name { "第二営業部" }
+    department_kana_name { "ダイニエイギョウブ" }
+    establishment_date { establishment_date }
+    abolished_date { abolished_date }
+  end
+
+  # 第ニ営業部　一課
+  factory :department_A_sales_department2_division1, class: Department do
+    department_code { "A01B01C02001" }
+    department_name { "第ニ営業部　一課" }
+    department_kana_name { "ダイニエイギョウブ　イッカ" }
+    establishment_date { establishment_date }
+    abolished_date { abolished_date }
+  end
+
+  # 第ニ営業部　ニ課
+  factory :department_A_sales_department2_division2, class: Department do
+    department_code { "A01B01C02002" }
+    department_name { "第ニ営業部　ニ課" }
+    department_kana_name { "ダイニエイギョウブ　ニカ" }
+    establishment_date { establishment_date }
+    abolished_date { abolished_date }
+  end
+
+  # B事業部
+  factory :department_B, class: Department do
+    department_code { "B01000000000" }
+    department_name { "B事業部" }
+    department_kana_name { "ビージギョウブ" }
     establishment_date { establishment_date }
     abolished_date { abolished_date }
   end
