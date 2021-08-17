bfeffdb3eb22efad66d10c9c66016c09d96d9be1
watson <chikuwa.wachi@gmail.com>
2021-08-18 08:23:05 +0900

    [change]子部署を追加するメソッドに循環する親子関係をエラーとする処理を追加。親子関係を取得するメソッド(処理は未記述)を追加。

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -2,24 +2,27 @@ require "rails_helper"
 
 RSpec.describe "部署モデルのテスト", type: :model do
   # ----- テストデータを登録
-  # 以下の階層構造の部署情報を想定する
-  # 以下の組織構造を持つ部署のテストデータ
-  #
-  # A事業部　A01000000000
-  # ┣営業部　A01B01000000
-  # ┃┗第一営業部　A01B01C01000
-  # ┃　┗営業一課　　A01B01C01001
-  let(:department_A) { FactoryBot.build(:department_A) }
-  let(:department_A_sales) { FactoryBot.build(:department_A_sales) }
-  let(:department_A_sales_department) { FactoryBot.build(:department_A_sales_department) }
-  let(:department_A_sales_department_division) { FactoryBot.build(:department_A_sales_department_division) }
+  # ※部署の階層構造は該当するFactoryBotのコメントを参照のこと
+  [
+    :department_A,
+    :department_A_sales,
+    :department_A_sales_department1,
+    :department_A_sales_department1_division1,
+    :department_A_sales_department1_division2,
+    :department_A_sales_department2,
+    :department_A_sales_department2_division1,
+    :department_A_sales_department2_division2,
+    :department_B,
+  ].each do |fb|
+    let(fb) { FactoryBot.build(fb) }
+  end
 
   context "テストデータの事前確認用テスト" do
     it "前提となるテストデータがバリデーションを通過すること" do
       expect(department_A).to be_valid
       expect(department_A_sales).to be_valid
-      expect(department_A_sales_department).to be_valid
-      expect(department_A_sales_department_division).to be_valid
+      expect(department_A_sales_department1).to be_valid
+      expect(department_A_sales_department1_division1).to be_valid
     end
   end
 
@@ -108,8 +111,18 @@ RSpec.describe "部署モデルのテスト", type: :model do
     end
   end
 
-  context "その他のテスト" do
-    it "[閉包テーブルモデルのテスト]add_childメソッドのテスト" do
+  context "add_childメソッドのテスト" do
+    it "既存の親子関係を反転したデータが登録されようとした場合にはエラーとなること" do
+      # 初期データとして親子関係を作成
+      department_A.add_child(department_A_sales)
+
+      # 初期データの親子関係を反転させたデータを作成
+      expect {
+        department_A_sales.add_child(department_A)
+      }.to raise_error("循環する親子関係は登録できません。子部署として追加しようとしている部署がすでに親として設定されていないか確認してください。")
+    end
+
+    it "階層テーブルに、自身親・子となっているレコードが登録されていること" do
       # 子部署を追加する
       department_A.add_child(department_A_sales)
 
@@ -119,7 +132,7 @@ RSpec.describe "部署モデルのテスト", type: :model do
       # 親子関係のレコード：1件
       expect(DepartmentHierarchy.count).to eq 3
 
-      # 親・子それぞれに自分自身の階層情報が閉包データとして登録されていること
+      # --- 自分自身の階層情報が閉包データとして登録されていること
       [
         department_A,
         department_A_sales,
@@ -129,6 +142,132 @@ RSpec.describe "部署モデルのテスト", type: :model do
 
       # --- 親子関係の閉包データが登録されていること
       expect(DepartmentHierarchy.where(parent_department: department_A, child_department: department_A_sales).exists?).to be_truthy
+    end
+  end
+
+  context "get_childsメソッドのテスト" do
+    skip "想定する親子関係を取得できること" do
+      # 以下の親子関係を作成する
+      # department_A
+      #   ┗department_A_sales
+      #     ┗department_A_sales_department1
+      #       ┗department_A_sales_department1_division1
+      #       ┗department_A_sales_department1_division2
+      #     ┗department_A_sales_department2
+      #       ┗department_A_sales_department2_division1
+      #       ┗department_A_sales_department2_division2
+      # department_B
+      #
+      department_A.add_child(department_A_sales)
+      department_A_sales.add_child(department_A_sales_department1)
+      department_A_sales_department1.add_child(department_A_sales_department1_division1)
+      department_A_sales_department1.add_child(department_A_sales_department1_division2)
+      department_A_sales_department2.add_child(department_A_sales_department2_division1)
+      department_A_sales_department2.add_child(department_A_sales_department2_division2)
+
+      # === 子部署が存在する部署の場合は、子部署のハッシュが返ってくること
+      # ---テスト対象部署:department_A
+      # [想定戻り値]
+      # {
+      #   department_A_sales => {
+      #     department_A_sales_department1 => {
+      #       department_A_sales_department1_division1 => {},
+      #       department_A_sales_department1_division2 => {},
+      #     },
+      #     department_A_sales_department2 => {
+      #       department_A_sales_department2_division1 => {},
+      #       department_A_sales_department2_division2 => {},
+      #     },
+      #   }
+      # }
+      h_department_A = department_A.get_childs
+      expect(
+        h_department_A[department_A_sales][department_A_sales_department1][department_A_sales_department1_division1]
+      ).to eq ({})
+      expect(
+        h_department_A[department_A_sales][department_A_sales_department1][department_A_sales_department1_division2]
+      ).to eq ({})
+      expect(
+        h_department_A[department_A_sales][department_A_sales_department2][department_A_sales_department2_division1]
+      ).to eq ({})
+      expect(
+        h_department_A[department_A_sales][department_A_sales_department2][department_A_sales_department2_division2]
+      ).to eq ({})
+
+      # ---テスト対象部署:department_A_sales
+      # [想定戻り値]
+      # {
+      #   department_A_sales_department1 => {
+      #     department_A_sales_department1_division1 => {},
+      #     department_A_sales_department1_division2 => {},
+      #   },
+      #   department_A_sales_department2 => {
+      #     department_A_sales_department2_division1 => {},
+      #     department_A_sales_department2_division2 => {},
+      #   },
+      # }
+      h_department_A_sales = department_A_sales.get_childs
+      expect(
+        h_department_A_sales[department_A_sales_department1][department_A_sales_department1_division1]
+      ).to eq ({})
+      expect(
+        h_department_A_sales[department_A_sales_department1][department_A_sales_department1_division2]
+      ).to eq ({})
+      expect(
+        h_department_A_sales[department_A_sales_department2][department_A_sales_department2_division1]
+      ).to eq ({})
+      expect(
+        h_department_A_sales[department_A_sales_department2][department_A_sales_department2_division2]
+      ).to eq ({})
+
+      # ---テスト対象部署:department_A_sales_department1
+      # [想定戻り値]
+      # {
+      #   department_A_sales_department1_division1 => {},
+      #   department_A_sales_department1_division2 => {},
+      # }
+      h_department_A_sales_department1 = department_A_sales_department1.get_childs
+      expect(
+        h_department_A_sales_department1[department_A_sales_department1_division1]
+      ).to eq ({})
+      expect(
+        h_department_A_sales_department1[department_A_sales_department1_division2]
+      ).to eq ({})
+
+      # ---テスト対象部署:department_A_sales_department2
+      # [想定戻り値]
+      # {
+      #   department_A_sales_department2_division1 => {},
+      #   department_A_sales_department2_division2 => {},
+      # }
+      h_department_A_sales_department2 = department_A_sales_department2.get_childs
+      expect(
+        h_department_A_sales_department2[department_A_sales_department2_division1]
+      ).to eq ({})
+      expect(
+        h_department_A_sales_department2[department_A_sales_department2_division2]
+      ).to eq ({})
+
+      # === 子部署が存在しない部署(独立した部署 or 末端部署)の場合は空のハッシュが返ってくること
+      # ---テスト対象部署:department_A
+      #                 department_A_sales_department1_division1
+      #                 department_A_sales_department1_division2
+      #                 department_A_sales_department2_division1
+      #                 department_A_sales_department2_division2
+      #                 department_B
+      # [想定戻り値]
+      # {}
+      [
+        department_A_sales_department1_division1,
+        department_A_sales_department1_division2,
+        department_A_sales_department2_division1,
+        department_A_sales_department2_division2,
+        department_B,
+      ].each do |d|
+        h = d.get_childs
+        expect(h.size).to eq 0
+        expect(h).to eq({})
+      end
     end
   end
 end
