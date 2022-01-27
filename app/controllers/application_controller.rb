19b4661e3dfe46190693259b085b012ece70af25
watson <chikuwa.wachi@gmail.com>
2022-01-28 08:12:02 +0900

    [change]cancancanの権限エラー発生時に、任意のViewに遷移させる処理を追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
     # current_user→current_employeeに変更
     @current_ability ||= Ability.new(current_employee)
   end
+
+  # ----- 権限が無いページにアクセスした場合に遷移させるページの設定
+  # Rails標準のrescue_fromメソッドで、cancancanにより発生した例外を取得し、
+  rescue_from CanCan::AccessDenied do |exception|
+    render "errors/403", status: 403
+  end
 end
