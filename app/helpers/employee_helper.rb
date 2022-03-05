e2bc0190c189b16934ac7cc166e39806c60de2bf
watson <chikuwa.wachi@gmail.com>
2022-03-05 11:55:48 +0900

    [change]社員一覧にソートアイコンを追加。

--- app/helpers/employee_helper.rb
+++ app/helpers/employee_helper.rb
@@ -17,9 +17,18 @@ module EmployeeHelper
   # * label
   # リンクボタンのラベル名
   def generate_sort_link(column:, label:)
+    # ----- リンクに埋め込むソートの昇順・降順を定義する
     # 昇順にソートされた場合は降順のリンクを、降順にソートされた場合は昇順のリンクを生成する
     # ※asc,desc以外の値の場合は明示的にascを設定する
     sort_direction = params[:sort_direction] == "asc" ? "desc" : "asc"
-    link_to(label, employees_list_path(sort_column: column, sort_direction: sort_direction))
+
+    # ----- 現在のソート状況を指し示すアイコンを生成する
+    sort_icon = icon("fas", "sort")
+    # 現在ソートされているカラムなら対応する矢印のアイコンに変更する
+    if column.to_s == params[:sort_column].to_s
+      sort_icon = sort_direction == "asc" ? icon("fas", "sort-up") : icon("fas", "sort-down")
+    end
+
+    sort_icon + link_to(label, employees_list_path(sort_column: column, sort_direction: sort_direction))
   end
 end
