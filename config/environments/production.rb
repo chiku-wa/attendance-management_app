fe43864450803903cb012e74612a5898f185248c
watson <chikuwa.wachi@gmail.com>
2022-03-27 18:06:25 +0900

    [change]SMTP設定を追加し、メールが送信されるように設定ファイルを見直し。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -51,7 +51,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -62,9 +62,41 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # ========== メール設定
+  # ----- deviseが送信するメールの本文内のURLのFQDN名
+  config.action_mailer.default_url_options = {
+    host: ENV["ACTION_MAILER_URL"],
+  }
+
+  # ----- SMTP設定
+  # メール送信時にエラーが発生した場合は例外を発生させる
+  config.action_mailer.raise_delivery_errors = true
+
+  # SMTPサーバの接続設定
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    # TLS認証を使用するか
+    enable_starttls_auto: true,
+
+    # SMTPサーバのアドレス
+    address: ENV["SMTP_ADDRESS"],
+
+    # SMTPサーバのポート番号
+    port: ENV["SMTP_PORT"],
+
+    # SMTPサーバのドメイン
+    # ※SMTPと通信を開始する際に使用するHELOドメイン、メールの中継ログ情報としてメールのヘッダに追加され
+    domain: ENV["SMTP_DOMAIN"],
+
+    # SMTPサーバのユーザ名
+    user_name: ENV["SMTP_USER_NAME"],
+
+    # SMTPサーバのパスワード
+    password: ENV["SMTP_PASSWORD"],
+
+    # SMTPサーバの認証方法
+    authentication: "plain",
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -81,9 +113,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
