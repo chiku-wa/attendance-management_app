d92aaf62a2a00b10e3938aed38702266e1150421
watson <chikuwa.wachi@gmail.com>
2022-04-16 12:33:52 +0900

    [change]システムテストに、パスワードリセットのメール受信のテストケースを追加。

--- spec/system/password_resets_spec.rb
+++ spec/system/password_resets_spec.rb
@@ -1,7 +1,63 @@
 require "rails_helper"
 
 RSpec.describe "パスワードリセット機能に関するシステムテスト", type: :system do
+  scenario "想定通りの件名、本文のパスワードリセットメールが送信されること" do
+
+    # ----- 事前準備
+    # ログインする社員情報を登録する
+    @role_common = FactoryBot.create(:role_common)
+    @employee = FactoryBot.create(:employee)
+    @employee.roles << @role_common
+
+    # ----- パスワードリセット操作を実施
+    # TOP画面(=ログイン画面)に遷移する
+    visit(root_path)
+
+    # パスワードリセット画面に遷移する
+    click_link("パスワードをお忘れの方はこちらをクリック")
+
+    # メールアドレスを入力してパスワードをリセットを実施
+    fill_in("employee_email", with: @employee.email)
+    click_button("パスワードをリセットする")
+
+    # ----- 画面上に想定通りのメッセージが表示されること
+    # ログイン画面に遷移し、メッセージが表示されること
+    expect(page).to(
+      have_title("ログイン")
+    )
+
+    expect(
+      find(:xpath, "//p[@id='message_notice']")
+    ).to have_content("パスワードの再設定を受け付けました。メールボックスをご確認ください。")
+
+    # ----- 受信したメールが期待どおりであることを確認する
+    mail = ActionMailer::Base.deliveries.last
+    # 差出人はSMTPユーザと同一であること
+    expect(mail.from).to eq(["noreply.attendance.mng@gmail.com"])
+    # 宛先は社員のメールアドレスであること
+    expect(mail.to).to eq([@employee.email])
+    # Ccは存在しないこと
+    expect(mail.cc).to be_nil
+  end
+
+  scenario "存在しないメールアドレスの場合はエラーが表示されること" do
+    # TOP画面(=ログイン画面)に遷移する
+    visit(root_path)
+
+    # パスワードリセット画面に遷移する
+    click_link("パスワードをお忘れの方はこちらをクリック")
+
+    # 何も入力せずにパスワードをリセットを実施
+    fill_in("employee_email", with: "invalid_address@example.com")
+    click_button("パスワードをリセットする")
+
+    expect(
+      find(:xpath, "//div[@id='error_explanation']")
+    ).to have_content("・メールアドレスは見つかりませんでした。")
+  end
+
   scenario "メールアドレスが未入力の場合はエラーが表示されること" do
+    # TOP画面(=ログイン画面)に遷移する
     visit(root_path)
 
     # パスワードリセット画面に遷移する
