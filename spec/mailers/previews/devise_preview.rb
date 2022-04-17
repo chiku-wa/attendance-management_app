2828cef755a20d120f15ebba656f973a198eb9cc
watson <chikuwa.wachi@gmail.com>
2022-04-17 13:31:55 +0900

    [change]メールのプレビュー確認用クラスを追加。

---
+++ spec/mailers/previews/devise_preview.rb
@@ -0,0 +1,17 @@
+#
+# deviseで送信するメールのプレビューを確認するためのクラス
+#
+class DevisePreview < ActionMailer::Preview
+
+  # ----------------------------------------------------
+  # # 概要
+  # パスワードリセットのメールをプレビューするためのメソッド
+  #
+  def reset_password_instructions
+    # 一般社員をサンプルとして抽出する
+    Devise::Mailer.reset_password_instructions(
+      Employee.includes([:roles]).where("roles.role_name" => I18n.t("master_data.role.common")).first,
+      Devise.friendly_token[0, 20],
+    )
+  end
+end
