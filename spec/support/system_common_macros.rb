ae47cc13db0f669ef174c2b874d52922df6be3b2
watson <chikuwa.wachi@gmail.com>
2022-08-27 12:09:38 +0900

    [change]社員の個別登録のシステムテストを追加。

--- spec/support/system_common_macros.rb
+++ spec/support/system_common_macros.rb
@@ -66,6 +66,124 @@ module SystemCommonMacros
 
   # ----------------------------------------------------
   # # 概要
+  # 引数として渡した一般社員の社員インスタンスをもとに社員新規登録画面(個別)にアクセスし、
+  # 必要な情報を入力する。
+  #
+  # # 引数
+  # * emoloyee
+  # 新規登録(個別)したい一般社員の社員モデルクラスのインスタンス
+  #
+  def fill_in_employee(employee:)
+    # ----- 前提として、社員の新規登録画面であることを確認する
+    expect(page).to(
+      have_title("社員登録")
+    )
+
+    # ----- 社員情報を入力
+    # テキストエリア入力
+    [
+      # メールアドレス
+      [:employee_email, employee.email],
+
+      # 社員コード
+      [:employee_employee_code, employee.employee_code],
+
+      # 社員名(姓
+      [:employee_employee_last_name, employee.employee_last_name],
+
+      # 社員名(名
+      [:employee_employee_first_name, employee.employee_first_name],
+
+      # 社員名カナ(姓
+      [:employee_employee_last_name_kana, employee.employee_last_name_kana],
+
+      # 社員名カナ(名
+      [:employee_employee_first_name_kana, employee.employee_first_name_kana],
+
+      # 年齢
+      [:employee_age, employee.age],
+
+      # パスワード
+      [:employee_password, employee.password],
+
+      # 確認用パスワード
+      [:employee_password_confirmation, employee.password],
+    ].each do |attribute_name, value|
+      fill_in(
+        attribute_name,
+        with: value,
+      )
+    end
+
+    # ドロップダウン入力
+    [
+      # 権限
+      # ※複数の権限が付与されている場合、最初の1件目を設定する
+      [employee.roles.first.role_name, "employee[role_ids]"],
+
+      # 就業状況
+      [employee.employment_status.status_name, "employee[employment_status_id]"],
+    ].each do |value, attribute_name|
+      # 就業状況
+      select(
+        value,
+        from: attribute_name,
+      )
+    end
+  end
+
+  # ----------------------------------------------------
+  # # 概要
+  # 社員情報登録画面で登録した情報が想定通りDBに登録されていることを確認する。
+  # 引数として渡した一般社員の社員インスタンスと、そのインスタンスと同じ社員コードを持つDBの
+  # 社員情報の情報が一致することを確認する。
+  #
+  # パスワードだけは、DBに格納された時点で暗号化されるため、実際にログインして確認する。
+  #
+  # # 引数
+  # * emoloyee
+  # 確認したい一般社員の社員モデルクラスのインスタンス
+  #
+  def matches_registered_employee(employee:)
+    # ----- 登録された社員情報を抽出
+    registred_employee = Employee.find_by(employee_code: employee.employee_code)
+
+    # ----- 引数の社員情報インスタンスと、登録されている社員情報が一致すること
+    [
+      # メールアドレス
+      :email,
+
+      # 社員コード
+      :employee_code,
+
+      # 社員名(姓)
+      :employee_last_name,
+
+      # 社員名(名)
+      :employee_first_name,
+
+      # 社員名カナ(姓)
+      :employee_last_name_kana,
+
+      # 社員名カナ(名)
+      :employee_first_name_kana,
+
+      # 年齢
+      :age,
+    ].each do |attribute_name|
+      expect(employee.send(attribute_name)).to eq registred_employee.send(attribute_name)
+    end
+
+    # ----- パスワードの確認のためにログインし直す
+    click_button("ログアウト")
+    login_macro(employee: employee)
+    expect(page).to(
+      have_title("メインメニュー")
+    )
+  end
+
+  # ----------------------------------------------------
+  # # 概要
   # パスワードリセット画面に遷移し、メールアドレスを入力して想定通りの画面が表示されることを確認する。
   #
   # # 引数
--- spec/system/employee_register_spec.rb
+++ spec/system/employee_register_spec.rb
@@ -26,9 +26,28 @@ RSpec.describe "社員の新規登録に関する画面のテスト", type: :sys
       .find_by("#{Role.table_name}.role_name" => I18n.t("master_data.role.common"))
     @employee_common.password = "pw_common"
     @employee_common.save
+
+    # ----- 社員登録画面で登録する社員情報(DB未保存の社員情報を抽出)
+    gimei = Gimei.name
+    @employee_eligible_for_registration = Employee.new(
+      employee_code: "A99999",
+      employee_last_name: gimei.last.kanji,
+      employee_first_name: gimei.first.kanji,
+      employee_last_name_kana: gimei.last.katakana,
+      employee_first_name_kana: gimei.first.katakana,
+      age: 30,
+      email: "test99999@example.com",
+      password: "foo_bar99999",
+      #「休職」
+      employment_status: EmploymentStatus.find_by(
+        status_code: I18n.t("master_data.employment_status.status_code.absences"),
+      ),
+    )
+    # 「一般社員」権限で登録する
+    @employee_eligible_for_registration.roles << Role.find_by(role_name: I18n.t("master_data.role.common"))
   end
 
-  scenario "システム管理者でログインしている場合は、社員の新規登録画面が表示されること" do
+  scenario "[社員個別登録]システム管理者でログインしている場合は、社員の新規登録画面が表示されること" do
     login_macro(employee: @employee_admin)
     visit(new_employee_registration_path)
 
@@ -37,7 +56,7 @@ RSpec.describe "社員の新規登録に関する画面のテスト", type: :sys
     )
   end
 
-  scenario "マネージャ、一般社員でログインしている場合は、社員の新規登録画面が表示されないこと" do
+  scenario "[社員個別登録]マネージャ、一般社員でログインしている場合は、社員の新規登録画面が表示されないこと" do
     [
       @employee_manager,
       @employee_common,
@@ -53,11 +72,40 @@ RSpec.describe "社員の新規登録に関する画面のテスト", type: :sys
     end
   end
 
-  scenario "ログインしていない場合は新規登録画面が表示されず、ログイン画面が表示されること" do
+  scenario "[社員個別登録]ログインしていない場合は新規登録画面が表示されず、ログイン画面が表示されること" do
     visit(new_employee_registration_path)
 
     expect(page).to(
       have_title("お探しのページは見つかりませんでした。")
     )
+  end
+
+  # scenario"[社員個別登録]ドロップダウンのデフォルト値が想定どおりであること" do
+  #   # ----- システム管理者でログインし、社員登録画面に遷移
+  #   login_macro(employee: @employee_admin)
+  #   visit(new_employee_registration_path)
+
+  #   # ----- ドロップダウンの値が想定どおりであること
+  #   # 権限
+
+  #   # 就業状況
+  # end
+
+  scenario "[社員個別登録]社員が正常に登録できること" do
+    # ----- システム管理者でログインし、社員登録画面に遷移
+    login_macro(employee: @employee_admin)
+    visit(new_employee_registration_path)
+
+    # ----- 社員登録情報を入力
+    fill_in_employee(employee: @employee_eligible_for_registration)
+
+    # ----- 社員情報を登録
+    click_button("社員登録")
+
+    # ----- 社員情報が想定通り登録できていること
+    matches_registered_employee(employee: @employee_eligible_for_registration)
+
+    # ----- システム管理者でログインされたままであること
+
   end
 end
