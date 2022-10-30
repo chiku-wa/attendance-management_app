d09d0c353abce02d9fb117452fa690820e13044b
watson <chikuwa.wachi@gmail.com>
2022-10-30 10:51:46 +0900

    [change]本務の登録上限をチェックするバリデーションを追加。

--- app/models/employee_department.rb
+++ app/models/employee_department.rb
@@ -1,4 +1,14 @@
 class EmployeeDepartment < ApplicationRecord
+  # =============== 定数
+  # 有効な本務・兼務を意味する離任日の固定値
+  END_DATE_FOR_ENABLED_AFFILIATION = Time.zone.parse("9999-12-31 23:59:59")
+
+  # 本務の上限は1とする
+  MAX_REGULAR = 1
+
+  # 兼務の上限は5とする
+  MAX_ADDITIONAL = 5
+
   # =============== 従属関係
   # 社員
   belongs_to :employee
@@ -25,20 +35,55 @@ class EmployeeDepartment < ApplicationRecord
   )
 
   # =============== カスタムバリデーション
-  validate :end_date_is_after_start_date
+  validate(
+    :end_date_is_after_start_date,
+    :enabled_regular_is_only_one,
+  )
 
   private
 
   # 着任日 >= 離任日の場合はバリデーションエラーとする
   def end_date_is_after_start_date
-    # 判定時にエラーとなるため、着任日、離任日のいずれかがnil値ならreutrnし、本バリデーションではチェックを行わない。
-    # ※presenceのValidationに一任するため
+    # 判定時にエラーとなるため、前提となるプロパティがnilならreutrnし、本バリデーションではチェックを行わない。
     return if start_date.blank? || end_date.blank?
 
     if (start_date.strftime("%Y%m%d").to_i >= end_date.strftime("%Y%m%d").to_i)
       errors.add(
         :start_date,
         I18n.t("activerecord.errors.models.employee_department.attributes.start_date.cannot_be_after_end_date"),
+      )
+    end
+  end
+
+  # 有効組織の本務が複数設定されている場合はバリデーションエラーとする
+  def enabled_regular_is_only_one
+    # 判定時にエラーとなるため、前提となるプロパティがnilならreutrnし、本バリデーションではチェックを行わない。
+    return if self.employee.blank? || self.affiliation_type.blank?
+
+    # すでに登録済みの有効な本務数を抽出する
+    registered_enabled_regular_department_size = EmployeeDepartment.joins(
+      :affiliation_type
+    ).where(
+      employee_id: self.employee.id,
+      affiliation_types: {
+        affiliation_type_name: I18n.t("master_data.affiliation_type.affiliation_type_name.regular"),
+      },
+      end_date: END_DATE_FOR_ENABLED_AFFILIATION,
+    ).count
+
+    # 登録しようとしている有効な本務数を抽出する
+    this_regular_department_size =
+      (self.affiliation_type.affiliation_type_name == I18n.t("master_data.affiliation_type.affiliation_type_name.regular") &&
+       self.end_date == END_DATE_FOR_ENABLED_AFFILIATION) ? 1 : 0
+
+    # 「(すでに登録済みの有効な本務数 + 登録しようとしている有効な本務数) > 許容する本務の数」ならエラーとする
+    if (registered_enabled_regular_department_size + this_regular_department_size) > MAX_REGULAR
+      errors.add(
+        :affiliation_type,
+        I18n.t(
+          "activerecord.errors.models.employee_department.attributes.affiliation_type.regular_is_only_one",
+          max_regular: MAX_REGULAR,
+        ),
       )
     end
   end
--- spec/models/employee_department_spec.rb
+++ spec/models/employee_department_spec.rb
@@ -2,16 +2,34 @@ require "rails_helper"
 
 RSpec.describe "社員-部署のテスト", type: :model do
   # ===== テストデータを登録
-  let(:employee_department_regular) { FactoryBot.build(:employee_department_regular) }
+  # ----- 所属種別テーブル
+  [
+    :affiliation_type_regular,
+    :affiliation_type_additional,
+  ].each do |fb|
+    let(fb) { FactoryBot.create(fb) }
+  end
+
+  # ----- 社員テーブル
+  let(:employee_work) { FactoryBot.build(:employee) }
+
+  # ----- 部署テーブル
+  let(:department_A) { FactoryBot.create(:department_A) }
 
   before do
-    # 外部キーのデータをsaveする(子テーブルのテストが正常に実行できないため)
-    employee_department_regular.save
+    # テストに使用するデータを登録
+    @employee_department_regular = EmployeeDepartment.new(
+      employee: employee_work,
+      department: department_A,
+      affiliation_type: affiliation_type_regular,
+      start_date: Time.zone.local(2021, 4, 1, 0, 0, 0),
+      end_date: Time.zone.local(9999, 12, 31, 23, 59, 59),
+    )
   end
 
   context "テストデータの事前確認用テスト" do
     it "前提となるテストデータがバリデーションを通過すること" do
-      expect(employee_department_regular).to be_valid
+      expect(@employee_department_regular).to be_valid
     end
   end
 
@@ -20,7 +38,7 @@ RSpec.describe "社員-部署のテスト", type: :model do
     # --- 社員のテスト
     it "社員がnilの場合はバリデーションエラーとなること" do
       valid_presence(
-        model: employee_department_regular,
+        model: @employee_department_regular,
         attribute: :employee,
       )
     end
@@ -29,7 +47,7 @@ RSpec.describe "社員-部署のテスト", type: :model do
     # --- 部署のテスト
     it "部署がnilの場合はバリデーションエラーとなること" do
       valid_presence(
-        model: employee_department_regular,
+        model: @employee_department_regular,
         attribute: :department,
       )
     end
@@ -38,7 +56,7 @@ RSpec.describe "社員-部署のテスト", type: :model do
     # --- 所属種別のテスト
     it "所属種別がnilの場合はバリデーションエラーとなること" do
       valid_presence(
-        model: employee_department_regular,
+        model: @employee_department_regular,
         attribute: :affiliation_type,
       )
     end
@@ -47,7 +65,7 @@ RSpec.describe "社員-部署のテスト", type: :model do
     # --- 着任日のテスト
     it "着任日がnilの場合はバリデーションエラーとなること" do
       valid_presence(
-        model: employee_department_regular,
+        model: @employee_department_regular,
         attribute: :start_date,
       )
     end
@@ -56,7 +74,7 @@ RSpec.describe "社員-部署のテスト", type: :model do
     # --- 離任日のテスト
     it "離任日がnilの場合はバリデーションエラーとなること" do
       valid_presence(
-        model: employee_department_regular,
+        model: @employee_department_regular,
         attribute: :end_date,
       )
     end
@@ -77,19 +95,47 @@ RSpec.describe "社員-部署のテスト", type: :model do
         # 部署の着任日 > 離任日 ※同日(日付をまたぐ)の場合
         -1.day,
       ].each do |diff|
-        employee_department_regular.start_date = twz
-        employee_department_regular.end_date = employee_department_regular.start_date + diff
-        expect(employee_department_regular).not_to be_valid
-        expect(employee_department_regular.errors.messages[:start_date]).to eq(
+        @employee_department_regular.start_date = twz
+        @employee_department_regular.end_date = @employee_department_regular.start_date + diff
+        expect(@employee_department_regular).not_to be_valid
+        expect(@employee_department_regular.errors.messages[:start_date]).to eq(
           ["離任日が着任日より未来日(着任日 < 離任日)となるようにしてください。"]
         )
       end
 
       # ----- バリデーションエラーにならないパターンのテスト
       # 部署の着任日 < 離任日 ※同日(日付をまたぐ)の場合はバリデーションエラーにならないこと
-      employee_department_regular.start_date = twz
-      employee_department_regular.end_date = employee_department_regular.start_date + 1.day
-      expect(employee_department_regular).to be_valid
+      @employee_department_regular.start_date = twz
+      @employee_department_regular.end_date = @employee_department_regular.start_date + 1.day
+      expect(@employee_department_regular).to be_valid
+    end
+
+    it "有効な本務が複数登録されようとした場合はバリデーションエラーとなり、想定通りのエラーメッセージが表示されること" do
+      # 本務を登録する
+      @employee_department_regular.save
+      expect(
+        EmployeeDepartment.where(
+          employee_id: @employee_department_regular.employee.id,
+          affiliation_type_id: affiliation_type_regular.id,
+        ).size
+      ).to eq 1
+
+      # 無効な本務(離任日≠9999/12/31 23:59:58)であれば、複数登録しようとしてもバリデーションエラーとならないこと
+      @employee_department_regular.end_date = Time.zone.parse("9999-12-31 23:59:58")
+      expect(@employee_department_regular).to be_valid
+
+      # 有効な本務(離任日=999/12/31)であれば、複数登録時にバリデーションエラーとなること
+      @employee_department_regular.end_date = Time.zone.parse("9999-12-31 23:59:59")
+      expect(@employee_department_regular).not_to be_valid
+      expect(@employee_department_regular.errors.messages[:affiliation_type]).to eq(
+        ["1人の社員には設定できる本務数は1個までです。"]
+      )
+
+      # TODO:
+      # DBの制約違反が発生すること
+      # expect {
+      #   model.save(validate: false)
+      # }.to raise_error(ActiveRecord::NotNullViolation)
     end
   end
 end
